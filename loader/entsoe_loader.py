846a871429b555b652ef1063e51ef105a8eecc90
Sebastian <sebastian@sebastians-macbook-air.home>
2026-08-12 12:54:24 +0200

    price loader

--- loader/entsoe_loader.py
+++ loader/entsoe_loader.py
@@ -1,6 +1,9 @@
 import os
 from entsoe import EntsoePandasClient
+from entsoe.exceptions import NoMatchingDataError
+
 from src.settings import settings
+import pandas as pd
 
 
 class EntsoeLoader:
@@ -11,3 +14,21 @@ class EntsoeLoader:
             raise ValueError("Environment variable ENTSOE_API_KEY is not set.")
         self.client = EntsoePandasClient(api_key=api_key)
         self.country = country
+
+    @staticmethod
+    def _prepare_dataframe(df: pd.DataFrame | pd.Series) -> pd.DataFrame:
+        if isinstance(df, pd.Series):
+            df = df.to_frame()
+
+        df.index = pd.to_datetime(df.index, utc=True)
+        return df.sort_index()
+
+    def get_prices(self, start: pd.Timestamp, end: pd.Timestamp) -> pd.DataFrame:
+        try:
+            prices = self.client.query_day_ahead_prices(
+                country_code=self.country, start=start, end=end
+            ).to_frame(name="price")
+            prices = prices.resample("15min").interpolate(method="time").round(2)
+            return self._prepare_dataframe(prices)
+        except NoMatchingDataError:
+            return pd.DataFrame(columns=["price"])
--- src/settings.py
+++ src/settings.py
@@ -10,4 +10,3 @@ class Settings:
 
 
 settings = Settings()
-
